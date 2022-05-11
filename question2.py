1fc5b43c8ea0964a0ba451fb9af2f275a895d250
aloneshed <aloneshed1@gmail.com>
2022-05-11 11:17:31 +0300

    adding query 4 and comments

--- question2.py
+++ question2.py
@@ -1,31 +1,37 @@
 import pandas as pd
 import openpyxl
 
-df = pd.read_csv('national-budget.csv')
+"""
+sources:
+https://www.youtube.com/watch?v=vmEHCJofslg
+"""
+
+df = pd.read_csv('national-budget.csv') # read the csv file
 
 
-def office_budget_by_year(office: str, year: int) -> int:
+# Helper function that get office name and year and return the total budget of the year
+def office_budget_by_year(office: str, year: int) -> float:
     relative_df = df.loc[(df['שם סעיף'] == office) & (df['שנה'] == year)]
     total_office_budget = relative_df['הוצאה נטו'].sum(axis=0)
     return total_office_budget
 
-
-def education_budget(year: int) -> int:
-    education_df = df.loc[(df['שם סעיף'] == 'משרד החינוך') & (df['שנה'] == year)]
-    total_education_budget = education_df['הוצאה נטו'].sum(axis=0)
+# Query 1
+def education_budget(year: int) -> float:
+    education_df = df.loc[(df['שם סעיף'] == 'משרד החינוך') & (df['שנה'] == year)] # filter the dataframe for education office and specific year
+    total_education_budget = education_df['הוצאה נטו'].sum(axis=0) # sum the column expenditure
     print(total_education_budget)
     return total_education_budget
 
-
+# Query 2
 def security_budget_ratio(year: int) -> float:
-    security_df = df.loc[(df['שם סעיף'] == 'משרד הבטחון') & (df['שנה'] == year)]
-    total_security_budget = security_df['הוצאה נטו'].sum(axis=0)
-    year_df = df.loc[df['שנה'] == year]
-    total_year_budget = year_df['הוצאה נטו'].sum(axis=0)
+    security_df = df.loc[(df['שם סעיף'] == 'משרד הבטחון') & (df['שנה'] == year)] # filter the dataframe for security office and specific year
+    total_security_budget = security_df['הוצאה נטו'].sum(axis=0) # sum the column expenditure
+    year_df = df.loc[df['שנה'] == year] # filter the dataframe for specific year
+    total_year_budget = year_df['הוצאה נטו'].sum(axis=0) # sum the total budget of specific year
     print(total_security_budget / total_year_budget)
-    return total_security_budget / total_year_budget
+    return total_security_budget / total_year_budget # return the security budget as a percentage of the total budget
 
-
+# Query 3
 def largest_budget_year(office: str) -> int:
     budget_list = []
     budget_by_year_df = pd.DataFrame()
@@ -36,4 +42,15 @@ def largest_budget_year(office: str) -> int:
     budget_by_year_df['bugdet'] = budget_list
     print(budget_by_year_df)
 
-largest_budget_year("משרד החינוך")
+
+# Query 4
+def ministry_of_Health_expenditure(year: int) -> float:
+    relative_df = df.loc[(df['שם סעיף'] == "משרד הבריאות") & (df['שנה'] == year) & (df["הוצאה/הכנסה"] == "הוצאה")]
+    total_office_budget = relative_df['הוצאה נטו'].sum(axis=0)
+    return total_office_budget
+
+
+# largest_budget_year("משרד החינוך")
+# print(office_budget_by_year("משרד הבריאות", 2007))
+# education_budget(2007)
+# print(ministry_of_Health_expenditure(2007))
